bd7f64a728afe713ab6dbc2281068debc4a2deba
Nininunz <ninicor11@gmail.com>
2025-09-05 08:10:15 -0500

    Apply Prettier formatting to source files

--- app/sitemap.js
+++ app/sitemap.js
@@ -3,17 +3,19 @@ import { getPageMap } from 'nextra/page-map'
 export default async function sitemap() {
   const pageMap = await getPageMap()
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
-  
+
   const extractRoutes = (items, parentPath = '') => {
     const routes = []
-    
+
     for (const item of items) {
       if (item.kind === 'Folder' && item.children) {
-        routes.push(...extractRoutes(item.children, `${parentPath}/${item.name}`))
+        routes.push(
+          ...extractRoutes(item.children, `${parentPath}/${item.name}`)
+        )
       } else if (item.kind === 'MdxPage') {
         const path = parentPath + (item.route || `/${item.name}`)
         const cleanPath = path.replace(/\/index$/, '') || '/'
-        
+
         routes.push({
           url: `${baseUrl}${cleanPath}`,
           lastModified: new Date(),
@@ -22,9 +24,9 @@ export default async function sitemap() {
         })
       }
     }
-    
+
     return routes
   }
-  
+
   return extractRoutes(pageMap)
 }
