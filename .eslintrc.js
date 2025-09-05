c95205ecc00ba72561a886b0733fb72f81fb1bab
Nininunz <ninicor11@gmail.com>
2025-09-04 22:00:39 -0500

    Add Prettier integration with ESLint for consistent code formatting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
-  extends: ['next/core-web-vitals'],
+  extends: ['next/core-web-vitals', 'prettier'],
+  plugins: ['prettier'],
   rules: {
+    // Prettier integration
+    'prettier/prettier': 'warn',
+
     // React/JSX specific rules
     'react/no-unused-prop-types': 'warn',
     'react/no-array-index-key': 'warn',
@@ -8,25 +12,35 @@ module.exports = {
     'react/jsx-pascal-case': 'error',
     'react/self-closing-comp': 'warn',
     'react/jsx-boolean-value': ['warn', 'never'],
-    
+
     // General code quality
     'no-console': 'warn',
     'no-debugger': 'error',
     'no-unused-vars': 'warn',
     'prefer-const': 'warn',
     'no-var': 'error',
-    
+
     // Import/export rules
-    'import/order': ['warn', {
-      groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-      'newlines-between': 'never'
-    }],
-    
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        'newlines-between': 'never',
+      },
+    ],
+
     // Accessibility
     'jsx-a11y/alt-text': 'error',
     'jsx-a11y/anchor-has-content': 'error',
     'jsx-a11y/no-redundant-roles': 'warn',
-    
+
     // Performance
     'react-hooks/exhaustive-deps': 'warn',
   },
@@ -35,6 +49,6 @@ module.exports = {
     'node_modules/**/*',
     'out/**/*',
     'public/**/*',
-    '*.config.js'
+    '*.config.js',
   ],
 }
