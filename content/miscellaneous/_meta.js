c2677ad82943e7ec40d03b9522a6a7d66c45a24d
Nininunz <ninicor11@gmail.com>
2025-09-06 18:10:25 -0500

    Update N52 engine failure content and add close-up valve image

--- content/miscellaneous/_meta.js
+++ content/miscellaneous/_meta.js
@@ -1,7 +1,7 @@
 import { withTodos } from '@/lib/meta-utils.js'
 
 const meta = {
-  'mysterious-n52-carbon-valves': 'The Mysterious BMW N52 Engine',
+  'mysterious-n52-carbon-valves': 'The Mysterious BMW N52 Engine Failure',
 }
 
 export default withTodos(meta, import.meta.url)
