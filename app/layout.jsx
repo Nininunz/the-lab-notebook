b29e3f19febe30861cc60649fa43468c0742e1db
Nininunz <ninicor11@gmail.com>
2025-09-08 08:54:26 -0500

    Remove banner and update repository base URL

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
 import { Footer, Layout, Navbar } from 'nextra-theme-docs'
-import { Banner, Head } from 'nextra/components'
+import { Head } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
 import { Logo } from '@/components/Logo'
 import 'nextra-theme-docs/style.css'
@@ -10,7 +10,6 @@ export const metadata = {
   // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
 }
 
-const banner = <Banner storageKey='some-key'>Nextra 4.0 is released 🎉</Banner>
 const navbar = (
   <Navbar
     logo={<Logo />}
@@ -36,10 +35,9 @@ export default async function RootLayout({ children }) {
       </Head>
       <body>
         <Layout
-          banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
-          docsRepositoryBase='https://github.com/shuding/nextra/tree/main/docs'
+          docsRepositoryBase='https://github.com/nininunz/the-lab-notebook/tree/main/'
           footer={footer}
           // ... Your additional layout options
         >
